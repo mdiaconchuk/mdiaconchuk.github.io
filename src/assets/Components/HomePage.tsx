66c5c936e1c913bd32ecf0e012f6ea9f8501e307
mdiaconchuk <mdiaconchuk@gmail.com>
2025-12-05 02:04:04 -0300

    Minor fixes + favicon

--- src/assets/Components/HomePage.tsx
+++ src/assets/Components/HomePage.tsx
@@ -1,6 +1,5 @@
 import Hero from "./Hero";
 import About from "./Sections/About";
-import Contact from "./Sections/Contact";
 import WorkSection from "./Sections/Works/WorkSection";
 
 function HomePage() {
